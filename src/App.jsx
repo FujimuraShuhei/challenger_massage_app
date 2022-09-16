8f5d1513e3b5c13baf98d8c56b1b1fe9cddd1845
FujimuraShuhei <shushu2525.1694@gmail.com>
2022-09-16 10:13:09 +0900

    ✨ 環境構築しました

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Chat } from 'stream-chat-react';
+import './App.css';
+
+const apiKey = '93wffnkjhc9k'
+
+function App() {
+  return (
+    <div className="app__wrapper">
+      <Chat>
+        <ChanellListContainer
+        
+        />
+      </Chat>
+    </div>
+  );
+}
+
+export default App;
